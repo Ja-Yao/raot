58a9afa83856a7e14e9fed3644d7ffc78aa5de95
Jacob Yao <jtyaos@gmail.com>
2025-07-05 22:43:57 -0400

    Change fallback component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
+import { LoaderCircle } from 'lucide-react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { Suspense } from 'react';
 import './App.css';
 import Map from './components/Map';
-import { Skeleton, Toaster } from './components/ui';
+import { Toaster } from './components/ui';
+
+const Fallback = () => {
+  return (
+    <div className='w-full h-full flex'>
+      <div className='grow'>
+        <LoaderCircle className='animate-spin h-12 w-12' />
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div id='main-container' className='h-full w-full overflow-clip'>
       <Toaster closeButton richColors position='bottom-center' />
-      <Suspense fallback={<Skeleton />}>
+      <Suspense fallback={<Fallback />}>
         <Map />
       </Suspense>
     </div>
