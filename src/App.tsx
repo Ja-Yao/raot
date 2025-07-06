28b2c550aa32ca1a2b8c081c5beaa7759d68ff10
Jacob Yao <jtyaos@gmail.com>
2025-07-05 21:53:29 -0400

    Remove unused state

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { Suspense, useState } from 'react';
-import type { Route } from 'types';
+import { Suspense } from 'react';
 import './App.css';
 import Map from './components/Map';
-import { Skeleton, Toaster } from './components/ui';
 import ThemeToggle from './components/ThemeToggle';
+import { Skeleton, Toaster } from './components/ui';
 
 function App() {
-  const [selectedLine, setSelectedLine] = useState<string>('Select a line...');
-  const [isLineSelectOpen, setIsLineSelectOpen] = useState(false);
-  const [routes, setRoutes] = useState<Route[]>([])
-
   return (
     <div id='main-container' className='h-full w-full overflow-clip'>
-      <Toaster closeButton richColors position='bottom-left' />
+      <Toaster closeButton richColors position='bottom-center' />
       <ThemeToggle className='absolute top-2 right-2 z-1' />
       <Suspense fallback={<Skeleton />}>
-        <Map setRoutes={setRoutes} />
+        <Map />
       </Suspense>
     </div>
   );
