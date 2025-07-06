a3b2dcd1cfbdedf9b9b83c10eda3900162128e2f
Jacob Yao <jtyaos@gmail.com>
2025-07-06 11:23:23 -0400

    Add JSDoc

--- src/api/stops.ts
+++ src/api/stops.ts
@@ -1,23 +1,30 @@
 import type { Stop } from 'types';
 import { MBTA_KEY } from './common';
 
+/**
+ * Gets the details of a stop
+ * @param stop_id The stop ID.
+ * @returns The stop attributes from the MBTA API. This should never return null due to how the MBTA handles relationships
+ */
 export async function getStop(stop_id: string): Promise<Stop> {
-    const response = await fetch(`/api/stops/${stop_id}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'X-API-Key': MBTA_KEY,
-      },
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
+  const response = await fetch(`/api/stops/${stop_id}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-API-Key': MBTA_KEY,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
 
-    const parsed = response
-      .json()
-      .then((json) => {return json.data.attributes})
-      .catch((error) => {
-        console.error('Error fetching shapes', error);
-        throw error;
-      });
-    return parsed;
+  const parsed = response
+    .json()
+    .then((json) => {
+      return json.data.attributes;
+    })
+    .catch((error) => {
+      console.error('Error fetching shapes', error);
+      throw error;
+    });
+  return parsed;
 }
